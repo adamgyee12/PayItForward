33e4e8dffd66f9007647db2a9fafee95be161eb3
Adam Yee <adamgyee@umich.edu>
2017-05-26 23:14:23 -0400

    changed linking

--- js/pagestartup.js
+++ js/pagestartup.js
@@ -1,13 +1,13 @@
 // Handle page transitions
 
 document.getElementById("enter").onclick = function () {
-  location.href = "http://localhost:8000/enter_token.html";
+  location.href = "https://adamgyee12.github.io/Portfolio/enter_token.html";
     //location.href = "http://localhost:8000/giotto.html";
     //https://adamgyee12.github.io/Portfolio/giotto.html
 };
 
 document.getElementById("track").onclick = function () {
-  location.href = "http://localhost:8000/track_tokens.html";
+  location.href = "https://adamgyee12.github.io/Portfolio/track_tokens.html";
     //location.href = "http://localhost:8000/giotto.html";
     //https://adamgyee12.github.io/Portfolio/giotto.html
 };
